c2affd93a6fb4221a586e5c0b25f35f5f44d2f48
Dmitry Kulagin <pkrstl@gmail.com>
2021-02-20 12:11:41 +0300

    Refactoring Code

--- TinkoffChat/Presentation/ProfileViewController.swift
+++ TinkoffChat/Presentation/ProfileViewController.swift
@@ -9,19 +9,65 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    private let userAvatarView = UIView()
-    private let userAvatarLabel = UILabel()
-    private let userAvatarImageView = UIImageView()
+    private let userAvatarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.894, green: 0.908, blue: 0.17, alpha: 1)
+        return view
+    }()
     
-    private let userNameLabel = UILabel()
-    private let userInfoLabel = UILabel()
+    private let userAvatarLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 120)
+        label.adjustsFontSizeToFitWidth = true
+        label.sizeToFit()
+        label.baselineAdjustment = .alignCenters
+        label.clipsToBounds = true
+        return label
+    }()
     
-    private let editButton = UIButton(type: .system)
+    private let userAvatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.sizeToFit()
+        return label
+    }()
+    
+    private let userDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let editButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.backgroundColor = UIColor(red: 0.965, green: 0.965, blue: 0.965, alpha: 1)
+        button.layer.cornerRadius = 14
+        return button
+    }()
     
     private var imagePicker: ImagePicker?
     
     private let userName = "Marina Dudarenko"
-    private let userInfo = "UX/UI designer, web-designer Moscow, Russia"
+    private let userDescription = "UX/UI designer, web-designer Moscow, Russia"
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -47,19 +93,20 @@ class ProfileViewController: UIViewController {
     }
     
     private func setupViews() {
+        view.backgroundColor = .white
         
         view.addSubview(userAvatarView)
         userAvatarView.addSubview(userAvatarLabel)
         userAvatarView.addSubview(userAvatarImageView)
         view.addSubview(userNameLabel)
-        view.addSubview(userInfoLabel)
+        view.addSubview(userDescriptionLabel)
         view.addSubview(editButton)
         
         userAvatarView.translatesAutoresizingMaskIntoConstraints = false
         userAvatarLabel.translatesAutoresizingMaskIntoConstraints = false
         userAvatarImageView.translatesAutoresizingMaskIntoConstraints = false
         userNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        userInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        userDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         editButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -83,10 +130,10 @@ class ProfileViewController: UIViewController {
             userNameLabel.leadingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             userNameLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
-            userInfoLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 32),
-            userInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userInfoLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 78),
-            userInfoLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -78),
+            userDescriptionLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 32),
+            userDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            userDescriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 78),
+            userDescriptionLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -78),
             
             editButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             editButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 62),
@@ -96,45 +143,14 @@ class ProfileViewController: UIViewController {
         
         userAvatarView.layoutIfNeeded()
         
-        view.backgroundColor = .white
-        
         userAvatarView.layer.cornerRadius = userAvatarView.frame.height / 2
-        userAvatarView.backgroundColor = UIColor(red: 0.894, green: 0.908, blue: 0.17, alpha: 1)
-        
-        userAvatarLabel.text = userName.getStringFirstChars()
-        userAvatarLabel.textAlignment = .center
-        userAvatarLabel.textColor = .black
-        userAvatarLabel.font = UIFont.boldSystemFont(ofSize: 120)
-        userAvatarLabel.adjustsFontSizeToFitWidth = true
-        userAvatarLabel.sizeToFit()
-        userAvatarLabel.baselineAdjustment = .alignCenters
-        userAvatarLabel.clipsToBounds = true
-        
-        userAvatarImageView.contentMode = .scaleAspectFill
-        userAvatarImageView.isUserInteractionEnabled = true
-        userAvatarImageView.addGestureRecognizer(UITapGestureRecognizer(target:self,
-                                                                         action: #selector(showImagePickerAlert)))
         userAvatarImageView.layer.cornerRadius = userAvatarImageView.frame.height / 2
-        userAvatarImageView.clipsToBounds = true
-        
-        userNameLabel.textColor = .black
-        userNameLabel.textAlignment = .center
-        userNameLabel.lineBreakMode = .byWordWrapping
-        userNameLabel.numberOfLines = 0
-        userNameLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        userAvatarImageView.addGestureRecognizer(UITapGestureRecognizer(target:self,
+                                                              action: #selector(showImagePickerAlert)))
+
+        userAvatarLabel.text = userName.getStringFirstChars()
         userNameLabel.text = userName
-        userNameLabel.sizeToFit()
-        
-        userInfoLabel.textColor = .black
-        userInfoLabel.font = UIFont.systemFont(ofSize: 16, weight: .light)
-        userInfoLabel.lineBreakMode = .byWordWrapping
-        userInfoLabel.numberOfLines = 0
-        userInfoLabel.text = userInfo
-        
-        editButton.setTitle("Edit", for: .normal)
-        editButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        editButton.backgroundColor = UIColor(red: 0.965, green: 0.965, blue: 0.965, alpha: 1)
-        editButton.layer.cornerRadius = 14
+        userDescriptionLabel.text = userDescription
     }
     
     @objc private func showImagePickerAlert() {
@@ -144,7 +160,6 @@ class ProfileViewController: UIViewController {
 }
 
 extension ProfileViewController: ImagePickerDelegate {
-
     func didSelect(image: UIImage?) {
         userAvatarImageView.image = image
         imagePicker = nil
