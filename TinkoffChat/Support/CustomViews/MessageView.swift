71934a63b35f961e36e12ef98789fb3591093b80
Dmitry Kulagin <pkrstl@gmail.com>
2021-03-21 20:57:30 +0300

    Added MessageView for Theme Switch, Added Send Message Icon

---
+++ TinkoffChat/Support/CustomViews/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  TinkoffChat
+//
+//  Created by Dmitry Kulagin on 21.03.2021.
+//
+
+import UIKit
+
+class MessageView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
