006a8a79cf6ca6ab51d63c0a1d71dd5c50ec2ac0
Dmitry Kulagin <pkrstl@gmail.com>
2021-04-09 17:32:24 +0300

    Removed Unused Logger in AppDelegate

--- TinkoffChat/App/AppDelegate.swift
+++ TinkoffChat/App/AppDelegate.swift
@@ -18,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        Log.show(message: "\(getTransitionInfo()), calls")
-        
         FirebaseApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -41,83 +39,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         
-        coreDataStack.didUpdateDataBase = { stack in
-            stack.printDatabaseStatistice()
-        }
-        
-        coreDataStack.enableObservers()
+        /*
+         coreDataStack.didUpdateDataBase = { stack in
+             stack.printDatabaseStatistice()
+         }
+         
+         coreDataStack.enableObservers()
+         */
         
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        Log.show(message: "\(getTransitionInfo()), calls")
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        Log.show(message: "\(getTransitionInfo()), calls")
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        Log.show(message: "\(getTransitionInfo()), calls")
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        Log.show(message: "\(getTransitionInfo()), calls")
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        Log.show(message: "\(getTransitionInfo()), calls")
-    }
-}
-
-extension AppDelegate {
-    
-    private func getTransitionInfo(method: String = #function) -> String {
-        
-        let fromState: String
-        let toState: String
-        
-        switch method {
-        case "application(_:didFinishLaunchingWithOptions:)":
-            fromState = "not running"
-            toState = getStateDescription()
-        case "applicationWillResignActive(_:)":
-            fromState = getStateDescription()
-            toState = "background"
-        case "applicationDidEnterBackground(_:)":
-            fromState = "active"
-            toState = getStateDescription()
-        case "applicationWillEnterForeground(_:)":
-            fromState = getStateDescription()
-            toState = "active"
-        case "applicationDidBecomeActive(_:)":
-            fromState = "background"
-            toState = getStateDescription()
-        case "applicationWillTerminate(_:)":
-            fromState = getStateDescription()
-            toState = "not running"
-        default:
-            fromState = "unknown"
-            toState = "unknown"
-        }
-
-        return "Application moved from \(fromState) to \(toState)"
-    }
-    
-    private func getStateDescription() -> String {
-        
-        let stateValue = UIApplication.shared.applicationState.rawValue
-
-        switch stateValue {
-        case 0:
-            return "active"
-        case 1:
-            return "inactive"
-        case 2:
-            return "background"
-        default:
-            return "unknown"
-        }
     }
 }
